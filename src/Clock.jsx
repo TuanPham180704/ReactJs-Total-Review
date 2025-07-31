6893909af56f2c81f39d3175cb2566e1e6d98b93
Tuandev18 <phamtuan180704@gmail.com>
2025-07-31 08:27:27 +0700

    componentDidMount

--- src/Clock.jsx
+++ src/Clock.jsx
@@ -1,5 +1,14 @@
 import React from 'react'
 
+const lists = ['BWM', 'TOYOTA', 'MERCEDES']
+
+const fetchAPI = () => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(lists)
+    }, 1000)
+  })
+}
 export default class Clock extends React.Component {
   constructor(props) {
     super(props)
@@ -10,32 +19,41 @@ export default class Clock extends React.Component {
       seconnds: {
         created: new Date().getSeconds()
       },
-      name : this.props.name
+      name: this.props.name,
+      lists: []
     }
     this.date = '30/07/2025'
     this.getTime = this.getTime.bind(this)
-   
   }
-  getTime(){
+  getTime() {
     const newState = {
       ...this.state,
       time: {
         created: new Date().toLocaleTimeString()
       },
-      second : {
+      seconnds: {
         created: new Date().getSeconds()
       }
     }
     this.setState(newState)
   }
-
+  componentDidMount() {
+    const seconds = document.getElementById('seconds')
+    console.log(seconds)
+    fetchAPI().then((res) =>
+      this.setState((prevState) => ({
+        ...prevState,
+        list: res
+      }))
+    )
+  }
   render() {
-    console.log(this.props); 
+    console.log(this.state)
     return (
       <div>
         <h3>Hello World ! {this.props.name}</h3>
-        <h4>Hom nay la {this.state.time.created}</h4>
-        <h4>Giay Thu : {this.state.second.created}</h4>
+        <h4 id='seconds'>Hom nay la {this.state.time.created}</h4>
+        <h4>Giay Thu : {this.state.seconnds.created}</h4>
         <button onClick={this.getTime}>Get Time</button>
       </div>
     )
