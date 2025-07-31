6407c1a790a17d85b09d68702517f4d38301bcba
Tuandev18 <phamtuan180704@gmail.com>
2025-07-31 16:45:48 +0700

    Cách dùng Props và PropTypes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import './App.css'
+import BareInput from './BareInput'
 import Clock from './Clock'
 import { useState } from 'react'
+import Layout from './Layout'
 
 function App() {
-  const [name, setName] = useState('Casio')
-  const [visible, setVisible] = useState(true)
+  // const [name, setName] = useState('Casio')
+  // const [visible, setVisible] = useState(true)
   return (
     <div className='App'>
-      <button onClick={() => setName('Apple')}>Chang Name</button>
+      {/* <button onClick={() => setName('Apple')}>Chang Name</button>
       <button onClick={() => setVisible(false)}>Chang Visible</button>
-      {visible && <Clock name={name} />}
+      {visible && <Clock name={name} />} */}
+      <Layout>
+        <h2>Hello Anh Em Cố Gắng Lên Nhé !!!</h2>
+        <BareInput type='password' name='input' value='hehe' onChange={() => {}} />
+      </Layout>
     </div>
   )
 }
