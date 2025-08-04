95460fec527381176e40f59a7e9d2d8ed02d7311
Tuandev18 <phamtuan180704@gmail.com>
2025-08-04 13:10:20 +0700

    React Hook useEffect

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
+import { useState } from 'react'
 import './App.css'
+import UserEffect from './ReactHookBasic/useEffect/UserEffect'
 import User from './ReactHookBasic/useStateHook/User'
 import UserClassComponent from './ReactHookBasic/useStateHook/User.class'
 
 
 function App() {
+  const [isShow,setIsShow] = useState(true)
   
   return (
     <div className='App'>
-      <UserClassComponent/>
-      <br/>
-      <User/>
+      {isShow && <UserEffect/>}
+      <button onClick={()=> setIsShow(false)}>Change Show Active</button>
     </div>
   )
 }
